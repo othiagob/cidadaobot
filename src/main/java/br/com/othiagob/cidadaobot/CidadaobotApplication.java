7fba42e9b256ca73051d36b2dfa60089d64ec7ed
Thiago B. <thiago.backend@gmail.com>
2026-05-01 03:49:53 -0400

    piloto

---
+++ src/main/java/br/com/othiagob/cidadaobot/CidadaobotApplication.java
@@ -0,0 +1,13 @@
+package br.com.othiagob.cidadaobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadaobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadaobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/othiagob/cidadaobot/CidadaobotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.othiagob.cidadaobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
